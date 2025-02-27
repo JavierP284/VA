421695889045a9a07ca38cb31938b3e713147690
JavierP284 <a22310284@ceti.mx>
2025-02-27 01:21:52 -0600

    Practica 6

--- Practica 6 prueba.py
+++ Practica 6 prueba.py
@@ -1,36 +1,29 @@
 import cv2
-import numpy as np
 
-# Cargar imagen en escala de grises
-imagen = cv2.imread("ladrillo.jpg", cv2.IMREAD_GRAYSCALE)
+orig = cv2.imread("Perro_salchicha.jpg")
+image = orig.copy()
+template = cv2.imread("Muestra.jpg")
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+methods = [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED, cv2.TM_CCORR,
+            cv2.TM_CCORR_NORMED, cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED]
 
-# Verificar que la imagen se haya cargado correctamente
-if imagen is None:
-    print("Error: No se pudo cargar la imagen.")
-    exit()
+for method in methods:
+    res = cv2.matchTemplate(image_gray, template_gray, method=method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    print(min_val, max_val, min_loc, max_loc)
 
-# Definir kernel
-kernel = np.array([[1, 1, 1], [-1, -1, -1], [0, 0, 0]], dtype=np.int32)
-
-# Obtener dimensiones de la imagen
-m, n = imagen.shape
-
-# Crear imagen de salida en ceros (misma dimensión que la imagen original)
-imagenf = np.zeros((m, n), dtype=np.uint8)
-
-# Aplicar la convolución manualmente
-for x in range(m - 2):
-    for y in range(n - 2):
-        region = imagen[x:x+3, y:y+3]  # Extraer región de 3x3
-        res = np.sum(region * kernel)  # Convolución (multiplicación y suma)
+    if method == cv2.TM_SQDIFF or method == cv2.TM_SQDIFF_NORMED:
+        x1, y1 = min_loc
+        x2, y2 = min_loc[0] + template.shape[1], min_loc[1] + template.shape[0]
+    else:
+        x1, y1 = max_loc
         
-        # Aplicar umbral
-        if res > 50:
-            imagenf[x, y] = 255  # Blanco
-        else:
-            imagenf[x, y] = 0  # Negro
+        x2, y2 = max_loc[0] + template.shape[1], max_loc[1] + template.shape[0]
 
-# Mostrar imagen filtrada
-cv2.imshow("Ladrillo Filtrado", imagenf)
-cv2.waitKey(0)
+    cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 3)
+    cv2.imshow("Image", image)
+    cv2.imshow("Template", template)
+    image = orig.copy()
+    cv2.waitKey(0)
 cv2.destroyAllWindows()

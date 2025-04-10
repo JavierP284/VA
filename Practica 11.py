61f3d9158d17e17fe81a495f485b62e9df624ad7
JavierP284 <a22310284@ceti.mx>
2025-04-10 17:09:03 -0600

    Practica 11

--- Practica 11.py
+++ Practica 11.py
@@ -5,9 +5,6 @@ imagen = cv2.imread('Tornillos.jpg')
 grises = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 bordes = cv2.Canny(grises, 100, 200)
 
-#Para OpenCV3
-#_, ctns, _ = cv2.findContours(bordes, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
 #Para OpenCV4
 ctns, _ = cv2.findContours(bordes, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
